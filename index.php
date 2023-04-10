767eb52ba0e48d87ede10b671ba13072041eb3aa
Sanjida Islam Shohely <66716089+shohely1710@users.noreply.github.com>
2023-04-10 19:23:59 +0600

    basic php from shikhun tutorial

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+//php documentation built in function
+
+$array = [
+    10, 20, 30 , 40, 50, 20, 30
+];
+
+$filtered_array = array_unique($array);
+
+echo count($array);
+
+
+
+
+
+    
+    
+
+
